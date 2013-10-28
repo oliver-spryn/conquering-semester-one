d7d8453ccc285ab17d3cd5f1ba7626b1a9c52a0e
Nick Tallents <nicktallents11@gmail.com>
2013-10-28 19:43:23 -0400

    adding hand functions flush and receive hand for zach

--- conquering-semester-one/Hand.h
+++ conquering-semester-one/Hand.h
@@ -18,6 +18,8 @@ public:
 	virtual int size() {return hand.size();}
 	virtual void addCard(CardGroup& group);
 	virtual vector<Card> removeCard(); //returns a vector<Card> of size 1 with the Card() if the user doesn't want to remove cards
+	vector<Card> flush(); //removes all of the cards
+	void receiveHand(Hand h); //receiving a hand from another player
 	bool hasTerrCard(string terrName);
 	void print();
 
